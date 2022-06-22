b2bd2bf2dc20271a657f5b0d234b61c78e4a32bf
Steve Webb <steve@humbly.com>
2022-06-22 18:37:51 -0500

    Store fid in localStorage if it appears in URL

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import { GlobalStateProvider } from 'globalState'
 import 'globalStyles'
 import './i18n'
 import { handleNetworkChange } from './utils/utils'
+import { setIdentifier } from './utils/affiliate'
 
 window.addEventListener('load', async () => {
   console.log('app config', process.env)
   const { client, networkId } = await handleNetworkChange()
+  setIdentifier()
   ReactDOM.render(
     <Suspense fallback={null}>
       <GlobalStateProvider>
